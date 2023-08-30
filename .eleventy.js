00a25a9ac8945130591556b94f30963b2d7ca868
Olafr9500 <olafr.af.munso@gmail.com>
2023-08-30 22:00:26 +0200

    Update path prefix right

--- .eleventy.js
+++ .eleventy.js
@@ -8,12 +8,12 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/assets/images");
     eleventyConfig.addPassthroughCopy({"node_modules/bootstrap-icons/font/bootstrap-icons.css":"assets/css/bootstrap-icons.css"});
     eleventyConfig.addPassthroughCopy({"node_modules/bootstrap-icons/font/fonts":"assets/css/fonts"});
-
     return {
         dir: {
             input: "src",
             layouts: "_layouts",
             output: "_site",
+            pathPrefix: "/11ty-exemple/"
         },
     };
 }
