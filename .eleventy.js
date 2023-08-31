95accac42f9adea4a5757e635427a856a0ae7a65
Vivien <vgondal@bspoke.fr>
2023-08-31 14:01:55 +0200

    add collections pages

--- .eleventy.js
+++ .eleventy.js
@@ -7,8 +7,12 @@ module.exports = function (eleventyConfig) {
 
     eleventyConfig.addPassthroughCopy("src/assets/images");
     eleventyConfig.addPassthroughCopy("src/assets/js");
-    eleventyConfig.addPassthroughCopy({"node_modules/bootstrap-icons/font/bootstrap-icons.css":"assets/css/bootstrap-icons.css"});
-    eleventyConfig.addPassthroughCopy({"node_modules/bootstrap-icons/font/fonts":"assets/css/fonts"});
+    eleventyConfig.addPassthroughCopy({ "node_modules/bootstrap-icons/font/bootstrap-icons.css": "assets/css/bootstrap-icons.css" });
+    eleventyConfig.addPassthroughCopy({ "node_modules/bootstrap-icons/font/fonts": "assets/css/fonts" });
+
+    eleventyConfig.addCollection("page", function(collections) {
+        return collections.getFilteredByTag("page");
+      });
 
     eleventyConfig.addFilter("currentYear", () => new Date().getFullYear().toString());
 
